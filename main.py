768fd7da88ea01353f49b413d116c2571cc60082
DarecekParecek <D.t.tunega@post.cz>
2023-05-30 22:06:39 +0200

    SortingAlgorithms

---
+++ main.py
@@ -0,0 +1,120 @@
+
+# Bubble Sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+# Selection Sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+
+# Quick Sort
+def quick_sort(arr):
+    def partition(arr, low, high):
+        pivot = arr[high]
+        i = low - 1
+
+        for j in range(low, high):
+            if arr[j] <= pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        return i + 1
+
+    def quick_sort_helper(arr, low, high):
+        if low < high:
+            pi = partition(arr, low, high)
+
+            quick_sort_helper(arr, low, pi - 1)
+            quick_sort_helper(arr, pi + 1, high)
+
+    quick_sort_helper(arr, 0, len(arr) - 1)
+
+
+# Heap Sort
+def heap_sort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            heapify(arr, n, largest)
+
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # Swap
+        heapify(arr, i, 0)
+
+
+# Merge Sort
+def merge_sort(arr):
+    def merge(arr, l, m, r):
+        n1 = m - l + 1
+        n2 = r - m
+
+        L = [0] * n1
+        R = [0] * n2
+
+        for i in range(n1):
+            L[i] = arr[l + i]
+
+        for j in range(n2):
+            R[j] = arr[m + 1 + j]
+
+        i = j = 0
+        k = l
+
+        while i < n1 and j < n2:
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < n1:
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < n2:
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    def merge_sort_helper(arr, l, r):
+        if l < r:
+            m = (l + r) // 2
+
+            merge_sort_helper(arr, l, m)
+            merge_sort_helper(arr, m + 1, r)
+
+            merge(arr, l, m, r)
+
+    merge_sort_helper(arr, 0, len(arr) - 1)
